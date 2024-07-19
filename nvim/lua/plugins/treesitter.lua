62fe12d645996c667fdc9ef37b2c92937931905d
Ken Jung <kgjung253@gmail.com>
2024-07-19 16:09:28 -0700

    local config inital commit

---
+++ nvim/lua/plugins/treesitter.lua
@@ -0,0 +1,21 @@
+local M = {
+  "nvim-treesitter/nvim-treesitter",
+  build = function()
+      require("nvim-treesitter.install").update({ with_sync = true })()
+  end,
+  config = function ()
+    local configs = require("nvim-treesitter.configs")
+
+    configs.setup({
+      ensure_installed = { "c", "lua", "vim", "vimdoc", "cpp"},
+      sync_install = false,
+      highlight = { 
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+      indent = { enable = true },
+    })
+  end
+}
+
+return { M }
