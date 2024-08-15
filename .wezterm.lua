1e0783b92198175006363f8c061946b06cd1efa7
Ken Jung <kgjung253@gmail.com>
2024-08-14 17:11:57 -0700

    tmux and wezterm updates

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,11 +1,12 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
+config.window_background_opacity = 0.9
+config.text_background_opacity = 1.0
 config.color_scheme = "OneHalfDark"
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.font_size = 14
-config.window_background_opacity = 2.5
 config.window_padding = {
   top = "64px",
 }
