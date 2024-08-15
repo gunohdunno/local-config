7e8c5aaa4c402388356ae8b722d5a34ce0e14aed
Ken Jung <kgjung253@gmail.com>
2024-08-14 17:21:57 -0700

    updated background in wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,9 +1,14 @@
 local wezterm = require("wezterm")
 
 local config = wezterm.config_builder()
-config.window_background_opacity = 0.9
+config.window_background_opacity = 0.8
 config.text_background_opacity = 1.0
-config.color_scheme = "OneHalfDark"
+local onehalfdark = wezterm.color.get_builtin_schemes()["OneHalfDark"]
+onehalfdark.background = "black"
+config.color_schemes = {
+  ["my onehalfdark"] = onehalfdark,
+}
+config.color_scheme = "my onehalfdark"
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.font_size = 14
